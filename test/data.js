0bdb6c4447295fd872c212274be25ceef2049533
paolo-delmundo <paolo.delmundo@opower.com>
2013-10-06 18:06:57 -0400

    Fixed deletion of package
    Removed the package when there were no more versions present.

--- test/data.js
+++ test/data.js
@@ -1,34 +1,10 @@
 var expect = require('chai').expect,
-  helpers = require('./helpers.js'),
-  proxyquire = require('proxyquire'),
-  Data = proxyquire('../lib/data.js',
-  {
-    'fs' : {
-      unlink: function(path, callback) {
-        callback();
-      }
-    }
-  });
+  testDataBuilder = require('./lib/testDataBuilder.js');
 
 describe('Data', function() {
   describe('index()', function() {
     it('reports one package', function() {
-      var registryData = new Data();
-      registryData._packageMap = {
-        'test-pkg' : {
-          versions: {
-            '0.0.1' : {
-              data: {
-                name: 'test-pkg',
-                version: '0.0.1',
-                _id: 'test-pkg@0.0.1'
-              },
-              pathToPackage: 'sandbox/test.tar.gz'
-            }
-          }
-        }
-      };
-
+      var registryData = testDataBuilder.givenDataWithSinglePackageVersion();
       var packages = registryData.index();
       expect(packages).to.be.length(1);
     });
@@ -36,20 +12,7 @@ describe('Data', function() {
 
   describe('deletePackage()', function() {
     it('deletes the package', function(done) {
-      var registryData = new Data();
-      registryData._packageMap = {
-        'test-pkg' : {
-          versions: {
-            '0.0.1' : {
-              data: {
-                name: 'test-pkg',
-                version: '0.0.1'
-              },
-              pathToPackage: 'sandbox/test.tar.gz'
-            }
-          }
-        }
-      };
+      var registryData = testDataBuilder.givenDataWithSinglePackageVersion('test-pkg', '0.0.1');
 
       registryData.deletePackage('test-pkg', '0.0.1', function() {
         var packages = registryData.index();
