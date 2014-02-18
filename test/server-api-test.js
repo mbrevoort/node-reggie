8ab9e970b202e03335b643998c4d51d2c330aaa7
leftieFriele <leftie.friele@gmail.com>
2014-02-18 15:22:34 +0100

    adding unit test for crash bug

---
+++ test/server-api-test.js
@@ -0,0 +1,14 @@
+var request = require('supertest');
+var helpers = require('./helpers.js');
+
+describe('GET /:name/-/:file', function(done) {
+    before(helpers.prepareSandbox);
+    before(helpers.startReggieServer);
+    after(helpers.stopReggieServer);
+
+    it('should respond with 404 when file does not exist', function(done) {
+        request(helpers.reggieUrl)
+            .get('/requirejs/-/requirejs-2.11.tgz')
+            .expect(404, done);
+    });
+});
